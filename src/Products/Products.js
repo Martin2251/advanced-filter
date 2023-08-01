d38ec466b692289399b47e2d5f1d41f721b7c739
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-01 07:27:24 +0100

    products

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -3,7 +3,19 @@ import './Products.css'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <>
+    <section className='card-container'>
+        <section className='card'>
+            <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe"  />
+            <div className='card-details'>
+                <h3 className='card-title'>Shoe</h3>
+                <section className='card-reviews'>
+                    
+                </section>
+            </div>
+        </section>
+    </section>
+    </>
   )
 }
 
