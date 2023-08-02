0c530358611ad19e1be9a6e2da262d86ab297f33
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-02 07:58:37 +0100

    css

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -8,14 +8,14 @@ const Products = () => {
     <>
     <section className='card-container'>
         <section className='card'>
-            <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe"  />
+            <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe"  className='card-img' />
             <div className='card-details'>
                 <h3 className='card-title'>Shoe</h3>
                 <section className='card-reviews'>
-                    <AiFillStar  />
-                    <AiFillStar  />
-                    <AiFillStar  />
-                    <AiFillStar  />
+                    <AiFillStar className='ratings-start' />
+                    <AiFillStar className='ratings-start' />
+                    <AiFillStar className='ratings-start' />
+                    <AiFillStar className='ratings-start' />
                     <span className='total-reviews'>4</span>
 
                 </section>
